6be88107de207c8062a4c89f621d2af74fee17c6
Victor Hugo Viana Santos <torugosis@gmail.com>
2021-03-22 20:03:19 -0300

    adiciona abertura de arquivos em tkinter

--- src/app.py
+++ src/app.py
@@ -1,10 +1,14 @@
 import tkinter as tk
+from tkinter import filedialog
+
+arquivo = filedialog.askopenfile()
+for line in arquivo:
+    print(line)
 
 root = tk.Tk()
 
-
-root_width = root.winfo_screenwidth() 
-root_height = root.winfo_screenheight() 
+root_width = root.winfo_screenwidth()
+root_height = root.winfo_screenheight()
 root.geometry("%dx%d+0+0" % (root_width, root_height))
 
 root.mainloop()
