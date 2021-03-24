add2f598d6b3bf8bff1674691902ac50dad6a0d3
Segatti <vittor_pc@outlook.com>
2021-03-24 01:31:13 -0400

    Teste de janela inicial
    Agora é possível buscar a imagem e abrir na janela

--- src/app.py
+++ src/app.py
@@ -1,15 +1,37 @@
-import tkinter as tk
+from tkinter import *
+from PIL import ImageTk, Image
 from tkinter import filedialog
-import cv2 as cv
+import os
+import funcoes
 
-arquivo = filedialog.askopenfile()
-cv.imread(arquivo.name)
-
-root = tk.Tk()
-
+root = Tk()
 root_width = root.winfo_screenwidth()
 root_height = root.winfo_screenheight()
 root.geometry("%dx%d+0+0" % (root_width, root_height))
 
+def openfn():
+    filename = filedialog.askopenfilename(title='open')
+    return filename
+def open_img():
+    x = openfn()
+    img = Image.open(x)
+    img = img.resize((250, 250), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(img)
+    panel = Label(root, image=img)
+    panel.image = img
+    panel.grid(row=1)
+
+btn = Button(root, text='abrir image', command=open_img).grid(row=0, column=0)
 
 root.mainloop()
+
+
+#arquivo = filedialog.askopenfile()
+#cv.imread(arquivo.name)
+
+#root = tk.Tk()
+
+#
+
+
+#root.mainloop()
